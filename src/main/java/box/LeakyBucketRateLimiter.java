e2a4a1fc50a2e5f33be3e3446e473baa45eb5a82
Ankush Arora <ankusharo@gmail.com>
2025-01-13 20:14:14 -0800

    Update

--- src/main/java/box/LeakyBucketRateLimiter.java
+++ src/main/java/box/LeakyBucketRateLimiter.java
@@ -15,6 +15,16 @@ Interruptible Locking	    Not supported	                    Supported
 Deadlock Management	        None	                            Can be managed explicitly
 Performance	                Can be slower in high contention	Often faster due to finer-grained control
 
+
+capacity = 10
+leakRate = 2 (2 requests per second)
+Initially, currentWaterLevel = 5
+A new request comes in after 3 seconds.
+elapsedTime = 3 seconds
+leakedWater = 3 * 2 = 6 requests leaked.
+currentWaterLevel = max(0, 5 - 6) = 0.
+
+
  */
 
 
